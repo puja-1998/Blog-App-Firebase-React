c7cac2cead0ffbdfa213475905ba68d9002360e1
Puja-1998 <ghodkepooja98@gmail.com>
2025-03-10 23:23:00 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 
-import { createBrowserRouter, RouterProvider, } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Login from './components/Login/Login';
-import Layout from './components/Layout/Layout';
+// import Layout from './components/Layout/Layout';
 import BlogList from './components/BlogList/BlogList';
 import CreateCard from './components/CreateCard/CreateCard';
 import { createContext, useState } from 'react';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />
-  },
-  {
-    path: "",
-    element: <Layout />,
-    children: [
-      {
-      path: "/home",
-      element:<BlogList />
-    },
-    
-    {
-      path: "/create",
-      element: <CreateCard />
-    },
-  ],
-  },
-]);
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 export const UserContext = createContext(null);
 
@@ -35,7 +15,15 @@ function App() {
   const [user, setUser] = useState({displayName: "", email: "", uid: ""})
   return (
     <UserContext.Provider value={{user, setUser}}> 
-      <RouterProvider router = {router} />
+      <BrowserRouter>
+              <Header />
+              <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/home" element={<BlogList />} />
+                <Route path="/create" element={<CreateCard />} />
+              </Routes>
+              <Footer />
+            </BrowserRouter>
     </UserContext.Provider>
   );
   
